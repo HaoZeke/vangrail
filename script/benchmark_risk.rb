a68eafdf9c62fb76f9b555051e8e556b121fe241
Rohit Goswami <rgoswami@ieee.org>
2026-08-20 09:13:04 -0500

    feat(performance): pin profiled artifacts

--- script/benchmark_risk.rb
+++ script/benchmark_risk.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'digest'
 require 'json'
 require 'open3'
 require 'optparse'
@@ -46,6 +47,7 @@ module Vangrail
         'scaling' => scaling,
         'scaling_analysis' => scaling_analysis(scaling),
         'parity' => parity(model),
+        'source_sha256' => source_digests,
         'artifacts' => artifacts(model),
         'process_memory_kib' => process_memory,
         'startup' => startup_measurement,
@@ -203,16 +205,49 @@ module Vangrail
 
     def artifacts(model)
       extension = $LOADED_FEATURES.detect { |path| File.basename(path).start_with?('vangrail_native.') }
+      extension = nil unless extension && File.file?(extension)
+      model_json = JSON.generate(model.to_h)
       {
         'core_library_bytes' => library_bytes,
-        'model_json_bytes' => JSON.generate(model.to_h).bytesize,
-        'native_extension_bytes' => extension && File.file?(extension) ? File.size(extension) : nil,
+        'core_library_sha256' => library_digest,
+        'model_json_bytes' => model_json.bytesize,
+        'model_json_sha256' => Digest::SHA256.hexdigest(model_json),
+        'native_extension_bytes' => extension && File.size(extension),
+        'native_extension_sha256' => extension && Digest::SHA256.file(extension).hexdigest,
       }
     end
 
     def library_bytes
+      library_files.sum { |path| File.size(path) }
+    end
+
+    def library_digest
       root = File.expand_path('..', __dir__)
-      Dir[File.join(root, 'lib', '**', '*')].select { |path| File.file?(path) }.sum { |path| File.size(path) }
+      digest = Digest::SHA256.new
+      library_files.each do |path|
+        relative = path.delete_prefix("#{root}/")
+        digest << relative << "\0" << Digest::SHA256.file(path).hexdigest << "\n"
+      end
+      digest.hexdigest
+    end
+
+    def library_files
+      root = File.expand_path('..', __dir__)
+      Dir[File.join(root, 'lib', '**', '*.{rb,json}')].sort
+    end
+
+    def source_digests
+      root = File.expand_path('..', __dir__)
+      {
+        'benchmark_risk' => file_digest(File.join(root, 'script', 'benchmark_risk.rb')),
+        'linear_model' => file_digest(File.join(root, 'lib', 'vangrail', 'linear_model.rb')),
+        'native_source' => file_digest(File.join(root, 'ext', 'vangrail_native', 'src', 'lib.rs')),
+        'native_lock' => file_digest(File.join(root, 'ext', 'vangrail_native', 'Cargo.lock')),
+      }
+    end
+
+    def file_digest(path)
+      Digest::SHA256.file(path).hexdigest
     end
 
     def process_memory
