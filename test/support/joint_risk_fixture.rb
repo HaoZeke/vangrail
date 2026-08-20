0fb517c27dc9414b9680099cd40f04a66b074c37
Rohit Goswami <rgoswami@ieee.org>
2026-08-20 04:46:17 -0500

    refactor(test): decompose joint artifact fixture

--- test/support/joint_risk_fixture.rb
+++ test/support/joint_risk_fixture.rb
@@ -17,29 +17,9 @@ module JointRiskFixture
       'coefficients' => { 'lexical.score' => 1.0, 'encoder.score' => 2.0 },
       'interactions' => { 'lexical.score*encoder.score' => 0.5 },
       'context_offsets' => { 'side:context' => 0.2, 'origin:data' => 0.1 },
-      'threat_model' => {
-        'training_composition' => { 'override' => 0.5, 'exfiltration' => 0.5 },
-        'log_likelihood_offsets' => { 'override' => Math.log(2.0), 'exfiltration' => Math.log(0.5) },
-        'covariance_diagonal' => { 'override' => 0.01, 'exfiltration' => 0.01 },
-      },
-      'covariance_diagonal' => {
-        'intercept' => 0.04,
-        'lexical.score' => 0.09,
-        'encoder.score' => 0.36,
-        'lexical.score*encoder.score' => 0.01,
-      },
-      'ood' => {
-        'feature_means' => { 'lexical.score' => 0.0, 'encoder.score' => 0.0 },
-        'feature_scales' => { 'lexical.score' => 1.0, 'encoder.score' => 1.0 },
-        'max_squared_distance' => 16.0,
-        'disagreement_rules' => [
-          {
-            'features' => %w[lexical.score encoder.score],
-            'max_standardized_difference' => 2.0,
-          },
-        ],
-        'calibration_valid_until' => '9999-12-31T23:59:59Z',
-      },
+      'threat_model' => threat_model_data,
+      'covariance_diagonal' => covariance_data,
+      'ood' => ood_data,
       'score_ranges' => {
         'lexical.score' => [-3.0, 3.0],
         'encoder.score' => [-3.0, 3.0],
@@ -52,6 +32,38 @@ module JointRiskFixture
       },
       'calibration' => { 'id' => 'calibration-v1', 'method' => 'identity' },
       'training_manifest_sha256' => 'a' * 64,
+    }
+  end
+
+  def threat_model_data
+    {
+      'training_composition' => { 'override' => 0.5, 'exfiltration' => 0.5 },
+      'log_likelihood_offsets' => { 'override' => Math.log(2.0), 'exfiltration' => Math.log(0.5) },
+      'covariance_diagonal' => { 'override' => 0.01, 'exfiltration' => 0.01 },
+    }
+  end
+
+  def covariance_data
+    {
+      'intercept' => 0.04,
+      'lexical.score' => 0.09,
+      'encoder.score' => 0.36,
+      'lexical.score*encoder.score' => 0.01,
+    }
+  end
+
+  def ood_data
+    {
+      'feature_means' => { 'lexical.score' => 0.0, 'encoder.score' => 0.0 },
+      'feature_scales' => { 'lexical.score' => 1.0, 'encoder.score' => 1.0 },
+      'max_squared_distance' => 16.0,
+      'disagreement_rules' => [
+        {
+          'features' => %w[lexical.score encoder.score],
+          'max_standardized_difference' => 2.0,
+        },
+      ],
+      'calibration_valid_until' => '9999-12-31T23:59:59Z',
     }
   end
 
